4aebe0197bbe21cf4a0cd840ff7fd9e1f9a68da3
acaiuserjuice <danieljamesgower@gmail.com>
2024-06-08 13:59:42 +0100

    -

---
+++ Quadratic Voting function test.py
@@ -0,0 +1,12 @@
+
+
+def quadratic_voting_interface():
+    proejcts = [
+        "Youth Empowerment and Mentorship Program",
+        "Community Safety and Policing Reform Initiative",
+        "Mental Health Support and Counseling Services Expansion",
+        "Education and Outreach Campaign on Conflict Resolution",
+        "Rehabilitation and Reintegration Program for Former Offenders"
+    ]
+
+    print('Thank you for participating in the test run for Quandratic Voting. \nFor more information on this project please check the README.')
