0a505e0271229a8eaa1ab8f01db42bc48c36d275
acaiuserjuice <danieljamesgower@gmail.com>
2024-06-13 14:27:04 +0100

    distribtion of minority behaviour example

---
+++ Graphs gitbook/distribution of behaviour.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# Parameters
+num_voters = 1000
+num_projects = 6
+max_credits = 100
+
+# Projects
+projects = ['Project A', 'Project B', 'Project C', 'Project D', 'Project E', 'Project F']
+
+# Simulate voting
+
+
+
+
+
+
+
+
+
+
